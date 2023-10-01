7aaeb5f430022e5329385876336d5c3206830187
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-10-01 20:53:21 +0200

    Update oraichain.sh

--- oraichain.sh
+++ oraichain.sh
@@ -1,111 +1,69 @@
-#!/bin/bash
-#
-# // Copyright (C) 2023 
-#
-
-echo -e "\033[0;32m"
-echo -e "\t\t\t Automatic Installer for Oraichain | Chain ID : Oraichain ";
-echo -e "\e[0m"
-
-# constant
-SOURCE=orai
-CHAIN_ID=Oraichain
-FOLDER=.oraid
-# VERSION=v0.41.4
-DENOM=orai
-REPO=https://github.com/oraichain/orai
-PORT=266
-
-# variable
-default="MONIKER NAME"
-read -p "Please enter your [$default]: " MONIKER
-MONIKER=${MONIKER:-$default}
-
-default="Oraichain_12715288.tar.lz4"
-echo -e "\e[1m\e[35mCheck voor new version:\e[0m https://snapshots.nysa.network/Oraichain/"
-read -p "Enter new  snapshot name [$default]: " SNAPSHOTS
-SNAPSHOTS=${SNAPSHOTS:-$default}
-
-echo "Verify the information below before proceeding with the installation!"
-echo ""
-echo -e "MONIKER        : \e[1m\e[35m$MONIKER\e[0m"
-echo -e "CHAIN ID       : \e[1m\e[35m$CHAIN_ID\e[0m"
-echo -e "NODE VERSION   : \e[1m\e[35m$VERSION\e[0m"
-echo -e "NODE FOLDER    : \e[1m\e[35m$FOLDER\e[0m"
-echo -e "NODE DENOM     : \e[1m\e[35m$DENOM\e[0m"
-echo -e "SOURCE CODE    : \e[1m\e[35m$REPO\e[0m"
-echo -e "NODE PORT      : \e[1m\e[35m$PORT\e[0m"
-echo -e "SNAPSHOTS      : \e[1m\e[35m$SNAPSHOTS\e[0m"
-echo ""
-
-read -p "Is the above information correct? (y/n) " choice
-if [[ $choice == [Yy]* ]]; then
-
-    echo "export SOURCE=${SOURCE}" 
-    echo "export WALLET=${MONIKER}" 
-    echo "export DENOM=${DENOM}" 
-    echo "export CHAIN_ID=${CHAIN_ID}" 
-    echo "export FOLDER=${FOLDER}"
-    echo "export VERSION=${VERSION}"
-    echo "export REPO=${REPO}" 
-    echo "export PORT=${PORT}"
-    source $HOME/.bash_profile
-
-else
-    echo "Installation cancelled!"
-    exit 1
-fi
-
+#🕌 andromeda
+🔑 sudo su | 🔑 sudo -i passwd root
+#💰 18-07-2023 t/m heden €15.85server + €0.61IPv4 = €16.46/pm Hetzner:CPX31-4VCPU-8GB-160GB
+#🪢 IPV4: 95.217.128.93
+#🪢 IPV6: 2a01:4f9:c012:7bda::1/64
+🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+#➣ Andromeda install date: 18-07-2023
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-
-#🏀 install recommended apps
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common lz4 jq -y
-
-#🏀 install docker and docker-compose
-wget -O docker-ubuntu.sh https://github.com/celik23/bash/raw/main/docker-ubuntu.sh && chmod +x docker-ubuntu.sh && ./docker-ubuntu.sh
-
-#🏀 Edit orai.env | docker-compose.yml
-curl -OL https://raw.githubusercontent.com/celik23/bash/main/docker-compose.yml && curl -OL https://raw.githubusercontent.com/celik23/bash/main/orai.env
-
-# find and replace
-sed -i -e "s/^USER *=.*/USER=$MONIKER/" $HOME/orai.env
-sed -i -e "s/^MONIKER *=.*/MONIKER=$MONIKER/" $HOME/orai.env
-sed -i -e "s/0.41.3/$VERSION/" $HOME/docker-compose.yml
-
-# Build and enter the container
-docker-compose pull && docker-compose up -d --force-recreate
-
-# Download Chain Data
-mkdir $HOME/.oraid
-curl -L https://snapshots.nysa.network/Oraichain/$SNAPSHOTS | tar -Ilz4 -xf - -C $HOME/.oraid
+# environment variables
+echo 'export PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[1;34m\]\h:\[\e[1;36m\]\w\[\e[1;35m\]\$\[\e[0m\] "' >> ~/.bash_profile
+echo 'export MONIKER="Andromeda"' >> ~/.bash_profile
+echo 'export CHAIN_ID=Oraichain' >> ~/.bash_profile
+echo 'export PASS="B!N453han@"' >> ~/.bash_profile
+echo 'export VERSION=v0.41.4' >> ~/.bash_profile
+source ~/.bash_profile
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-docker exec -it orai_node /bin/bash -c 'oraid init $MONIKER --chain-id "$CHAIN_ID"'
-#⛔ oraid keys add $MONIKER 2>&1 | tee account.txt && exit
-#       👆 OR 👇
-#👉 import exist wallet (recover wallet)
-docker exec -it orai_node /bin/bash -c 'oraid keys add $MONIKER --recover'
-
+#👉 1. firewall settup
+sudo apt update -y
+sudo apt install ufw -y
+sudo ufw allow 22,443,26656:26657/tcp
+sudo ufw --force enable 
+sudo ufw status verbose
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-# Download genesis.json
-wget -O $HOME/.oraid/config/genesis.json https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/genesis.json
+# https://github.com/celik23/bash
+wget -O $HOME/oraichain.sh https://github.com/celik23/bash/raw/main/oraichain.sh && chmod +x $HOME/oraichain.sh && $HOME/oraichain.sh
 
-# Netwrok setup | Replace config.toml and app.toml
-wget -O $HOME/oraichain-network.sh https://github.com/celik23/bash/raw/main/oraichain-network.sh && chmod +x oraichain-network.sh && $HOME/oraichain-network.sh
+# Input:
+➣ 'Moniker name :' Andromeda
+➣ 'Snapshot name:' Oraichain_13715321.tar.lz4
+🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+#🏀 Go docker container manual install 🏀
+
+#Enter keyring passphrase: <keypass>
+- name: Andromeda
+  type: local
+  address: orai1425a2jysn9lkacvuwmde89vxqlpxa0xd2p28xt
+  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Atv636/+gajhJ/Fa1aJld1F4j7Zz5HfdMe9qMSuPwOP5"}'
+  mnemonic: ""
+  Operator-address: oraivaloper1425a2jysn9lkacvuwmde89vxqlpxa0xdrt3wdv
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-#🛑 screen -ls #list-session | screen -S <new-session-name> | screen -r <session-name> | screen -d -r 27863
-# screen -S home
-# docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start'
-docker-compose restart orai && docker-compose exec orai bash -c 'oraivisor start --p2p.pex false --p2p.persistent_peers "e6fa2f222236a9ca5e10b238de87eb12497a649c@167.99.119.182:26656,911b290c59a4d3248534b53bdbc8dd4615bb5870@167.99.119.182:26656,35c1f999d67de56736b412a1325370a8e2fdb34a@5.189.169.99:26656,5ad3b29bf56b9ba95c67f282aa281b6f0903e921@64.225.53.108:26656"'
+#🛑 screen -ls <list-session> | screen -S <new-session-name> | screen -r <session-name> | screen -d -r 27863 <deatach>
+screen -S home
+docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start'
+docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start --p2p.pex false --p2p.persistent_peers "e6fa2f222236a9ca5e10b238de87eb12497a649c@167.99.119.182:26656,911b290c59a4d3248534b53bdbc8dd4615bb5870@167.99.119.182:26656,35c1f999d67de56736b412a1325370a8e2fdb34a@5.189.169.99:26656,5ad3b29bf56b9ba95c67f282aa281b6f0903e921@64.225.53.108:26656"'
 #✋ 🍺🍺🍺 WAIT UNTIL YOUR NODE IS SYNCHRONIZED 🍺🍺🍺
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
 #👉 Recover node information
-#🛑 /mnt/orai/.oraid/config/priv_validator_key.json
-#🛑 /mnt/orai/.oraid/config/node_key.json
+#🛑 /root/orai/.oraid/config/priv_validator_key.json
+#🛑 /root/orai/.oraid/config/node_key.json
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-#       👆 OR 👇
+#                   👆 OR 👇
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
 #🚀 Create validator transaction (inside of the container)
-#    make 2orai over naar de Vallet
-#⛔ docker-compose exec orai bash
-#⛔ wget -O /usr/bin/fn https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/fn.sh && chmod +x /usr/bin/fn && fn createValidator
+#🚀 make 2orai over to the Vallet
+#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+docker-compose exec orai bash
+wget -O /usr/bin/fn https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/fn.sh && chmod +x /usr/bin/fn && fn createValidator
+#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+#🛑 Edit validators
+oraid tx staking edit-validator \
+--chain-id $CHAIN_ID \
+--new-moniker $MONIKER \
+--details "In the ever-expanding universe of blockchain technology, Andromeda emerges as a guiding star, illuminating the path towards secure and decentralized consensus. As a validator node, Andromeda plays a pivotal role in ensuring the integrity and reliability of blockchain projects." \
+--website "-" \
+--from $USER \
+--fees 250orai \
+--commission-rate=0.03 \
+-y
 #🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
