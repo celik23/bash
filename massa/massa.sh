f484bb8b890ed959a10f29beab8d51ddd45c45be
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-10-04 13:44:34 +0200

    Add files via upload

---
+++ massa/massa.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# // Copyright (C) 2023 
+#
+
+<<comment
+# Official accounts:
+https://docs.massa.net/docs/node/home
+https://twitter.com/massalabs - Massa Official Twitter
+https://twitter.com/Massadopted1 - Massadopted Official Twitter
+https://t.me/massanetwork - Massa Official Telegram
+https://t.me/massa_turkey - Turkish Speaking Telegram 
+https://twitter.com/MassaTurkiye - Turkish Speaking Community Twitter
+comment
+#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰
+# Environment variables 🍒
+echo 'export PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[1;34m\]\h:\[\e[1;36m\]\w\[\e[1;35m\]\$\[\e[0m\] "' >> ~/.bash_profile
+echo 'export password=B!N453han@' >> ~/.bash_profile 
+source ~/.bash_profile 
+#🔖 —————————————————————————————————————
+#🏁 Install necessary dependencies/requirements
+sudo apt update
+sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev cmake screen cron nano
+
+OS="linux.tar" # or "linux_arm64.tar"
+URL="https://api.github.com/repos/massalabs/massa/releases/latest"
+URL=$(curl -s ${URL} | grep "browser_download_url" | cut -d '"' -f 4 | grep ${OS})
+latest="$(basename -a ${URL})" && wget "${URL}" -O $HOME/${latest}
+tar -xvf ${latest} -C $HOME/ && rm $HOME/${latest}
+chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client 
+echo -e -n "${msg}" >> "${latest}.txt" 
+
+sudo tee /root/massa/massa-node/config/config.toml > /dev/null <<EOF
+[network]
+routable_ip ="$(curl ifconfig.co)"
+
+[bootstrap]
+max_ping = 10000
+EOF
+sudo nano /root/massa/massa-node/config/config.toml
+
+<<comment
+		# Resotre node file(s) and # restart node
+		# wallet_generate_secret_key 	#not needed/only new wallet create#
+		wallet_add_secret_keys {Secret key}	#import (wallet) secret_keys
+		get_status
+		wallet_info
+comment
+🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰
+#👉  Ctrl + A C #new window || Ctrl + A P #Switch andere Node
+#⛔ screen -S home 
+#⛔ cd ~/massa/massa-node/ && ./massa-node -p 'B!N453han@' | & tee logs.txt
+cd $HOME/massa/massa-client/ && ./massa-client -p "${password}"
+🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰
+👉 services
+sudo tee /etc/systemd/system/massad.service > /dev/null <<EOF
+[Unit]
+Description=Massa Daemon
+After=network-online.target
+
+[Service]
+Environment="RUST_BACKTRACE=full"
+WorkingDirectory=$HOME/massa/massa-node
+User=$USER
+ExecStart=$HOME/massa/massa-node/massa-node -p "${password}"
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload 
+systemctl enable massad 
+
+systemctl start massad 
+systemctl status massad
+systemctl stop massad
+
+systemctl restart massad && journalctl -u massad -f -o cat 
+journalctl --unit=massad.service -n 10 --no-pager
+
