99c623678b7cea7387d44c5621afb76a026db106
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-10-07 06:38:44 +0200

    Update hopr.sh

--- hopr/hopr.sh
+++ hopr/hopr.sh
@@ -13,9 +13,13 @@ default="moduleAddress"
 read -p "Please enter your [$default]: " moduleAddress
 moduleAddress=${moduleAddress:-$default}
 
-default="host"
+default="host-ipaddress"
 read -p "Please enter your [$default]: " host
 host=${host:-$default}
+
+apiToken='YOUR_SECURITY_TOKEN'
+read -p "Please enter your [$default]: " apiToken
+apiToken=${apiToken:-$default}
 
 echo "Verify the information below before proceeding with the installation!"
 echo ""
@@ -39,24 +43,22 @@ fi
 # Install docker
 wget -O docker-ubuntu.sh https://raw.githubusercontent.com/celik23/bash/main/oraichain/docker-ubuntu.sh && chmod +x docker-ubuntu.sh && ./docker-ubuntu.sh
 
-# Install HOPRd without Grafana (-m 4g)
+# INSTALL AND RUN HOPRd (-m 4g)
 docker run --pull always --restart on-failure -m 8g \
-  --platform linux/x86_64 --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 \
-  -ti -v $HOME/.hoprd-db-dufour:/app/hoprd-db \
-  -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 \
-  -e DEBUG="hopr*" europe-west3-docker.pkg.dev/hoprassociation/docker-images/hoprd:providence \
-  --network dufour --init --api \
-  --identity /app/hoprd-db/.hopr-id-dufour \
-  --data /app/hoprd-db \
-  --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' \
-  --apiHost "0.0.0.0" \
-  --apiToken 'MyS3cur1tyT0ken' --healthCheck \
-  --healthCheckHost "0.0.0.0" --announce \
-  --safeAddress ${safeAddress} \
-  --moduleAddress ${moduleAddress} \
-  --host ${host}:9091
+    --platform linux/x86_64 --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 \
+    -ti -v $HOME/.hoprd-db-dufour:/app/hoprd-db \
+    -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 \
+    -e DEBUG="hopr*" europe-west3-docker.pkg.dev/hoprassociation/docker-images/hoprd:providence \
+    --network dufour --init --api \
+    --identity /app/hoprd-db/.hopr-id-dufour \
+    --data /app/hoprd-db \
+    --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' \
+    --apiHost "0.0.0.0" \
+    --healthCheckHost "0.0.0.0" --announce \
+    --apiToken ${apiToken} --healthCheck \
+    --safeAddress ${safeAddress} \
+    --moduleAddress ${moduleAddress} \
+    --host ${host}:9091
 
 
-#👉 Backup/Recover Identity-file
-# /root/.hoprd-db-monte-rosa/.hopr-id-monte-rosa
-# restart hoprd.service
+#end
