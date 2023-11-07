db1f9321fc3d65307d5528cd53dbeecce972a003
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-11-07 09:30:04 +0100

    Update oraichain.sh

--- oraichain/oraichain.sh
+++ oraichain/oraichain.sh
@@ -38,7 +38,6 @@ echo "${NC}"
 
 read -p "Is the above information correct? (y/n) " choice
 if [[ $choice == [Yy]* ]]; then
-    echo 'export PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]\[\e[38;5;172m\]\u\[\e[m\]@\[\e[1;34m\]\h:\[\e[1;36m\]\w\[\e[1;35m\]\$\[\e[0m\] "' >> ~/.bash_profile
     echo "export MONIKER=${MONIKER}" >> $HOME/.bash_profile
     echo "export CHAIN_ID=${CHAIN_ID}" >> $HOME/.bash_profile
     echo "export FOLDER=${FOLDER}" >> $HOME/.bash_profile
