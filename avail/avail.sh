a9981259321bb82e8cf6dd624ba688d4a2118dae
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-12-12 16:36:13 +0100

    Update avail.sh

--- avail/avail.sh
+++ avail/avail.sh
@@ -3,7 +3,7 @@
 echo -e "\e[1;33m           A V A I L  B I N A R Y  I N S T A L L   F O R   U B U N T U   2 3 . 0 4\e[0m"
 
 # variable / input
-default=$ALIAS
+default=${ALIAS}
 read -p "Please enter your ALIAS NAME [$default]: " ALIAS
 ALIAS=${ALIAS:-$default}
 
