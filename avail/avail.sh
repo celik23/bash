738db000c2ad3c4418f20a81902144464abc5fd0
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-12-12 20:50:25 +0100

    Update avail.sh

--- avail/avail.sh
+++ avail/avail.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo -e "\n\e[1;33m           A V A I L  B I N A R Y  I N S T A L L   F O R   U B U N T U   2 3 . 0 4"
-echo -e "\e[0m"
-
+echo -e "\n${YL}           A V A I L  B I N A R Y  I N S T A L L   F O R   U B U N T U   2 3 . 0 4${NC}"
+YL="\e[1;33m"
+NC="\e[0m"
 # variable / input
 TAG=2304
 
@@ -15,8 +15,8 @@ read -p "Please enter your ALIAS-NAME [$default]: " ALIAS
 ALIAS=${ALIAS:-$default}
 
 echo -e "Verify the information below before proceeding with the installation!\n"
-echo -e "ALIAS        : $ALIAS"
-echo -e "VERSION      : $TAG"
+echo -e "ALIAS        : ${YL}$ALIAS${NC}"
+echo -e "VERSION      : ${YL}$TAG${NC}"
 echo -e "\n"
 
 read -p "Is the above information correct? (y/N) " choice
@@ -41,9 +41,6 @@ sudo apt install git curl screen -y
 # Download and extract binary
 wget https://github.com/availproject/avail/releases/download/v1.8.0.4/x86_64-ubuntu-${TAG}-data-avail.tar.gz
 tar xvzf x86_64-ubuntu-2304-data-avail.tar.gz && rm ./x86_64-ubuntu-${TAG}-data-avail.tar.gz
-
-# run avail or start from services
-# /root/data-avail -d ./output --chain goldberg --validator --name "$ALIAS"
 
 # Make service
 sudo tee /etc/systemd/system/availd.service > /dev/null <<EOF
@@ -73,6 +70,6 @@ systemctl start availd
 #You can check that it''s working with:
 systemctl status availd
 
-echo -e "\n\e[1;33m For break 'journalctl' Ctrl + C\n\e[0m"
+echo -e "\n${YL} For break 'journalctl' Ctrl + C\n${NC}"
 journalctl -f -u availd
 
