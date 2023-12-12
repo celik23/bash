45f9b86ae87bb1e9d9680da86e1803b93472d715
Gok Tengri <61687287+celik23@users.noreply.github.com>
2023-12-12 16:32:17 +0100

    Update avail.sh

--- avail/avail.sh
+++ avail/avail.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo -e "\e[1;33m"
-echo "           A V A I L  B I N A R Y  I N S T A L L   F O R   U B U N T U   2 3 . 0 4"
+echo -e "\e[1;33m           A V A I L  B I N A R Y  I N S T A L L   F O R   U B U N T U   2 3 . 0 4\e[0m"
 
 # variable / input
 default=$ALIAS
