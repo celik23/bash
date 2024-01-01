1ef6c2100eb4f13584ca2dc324e6b269038f0ee3
Gok Tengri <61687287+celik23@users.noreply.github.com>
2024-01-01 07:22:47 +0100

    Update oraichain-binary.sh

--- oraichain/oraichain-binary.sh
+++ oraichain/oraichain-binary.sh
@@ -29,7 +29,7 @@ echo -e "${GREEN}Check voor new snapshot version: https://snapshots.nysa.network
 read -p "Enter new snapshot name [$default]: " SNAPSHOTS
 SNAPSHOTS=${SNAPSHOTS:-$default}
 
-echo -e "\nVerify the information below before proceeding with the installation!\n"
+echo -e "${CYAN}\nVerify the information below before proceeding with the installation!\n${NC}"
 echo -e "NODENAME       : ${GREEN}$NODENAME${NC}"
 echo -e "CHAIN ID       : ${GREEN}$CHAIN_ID${NC}"
 echo -e "NODE VERSION   : ${GREEN}$VERSION${NC}"
