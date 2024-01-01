4d55bec064ac1a19b487825af1c6fd76ebd4fb4d
Gok Tengri <61687287+celik23@users.noreply.github.com>
2024-01-01 21:18:00 +0100

    Update oraichain.sh

--- oraichain/oraichain.sh
+++ oraichain/oraichain.sh
@@ -51,11 +51,13 @@ sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-p
 
 #🏀 Edit orai.env & docker-compose.yml
 curl -OL https://raw.githubusercontent.com/celik23/bash/main/oraichain/docker-compose.yml && curl -OL https://raw.githubusercontent.com/celik23/bash/main/oraichain/orai.env
+curl -o docker-compose.yml https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/docker-compose.0.41.4.yml
+curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/orai.env
 
 # find and replace
 sed -i -e "s/^USER *=.*/USER=$NODENAME/" $HOME/orai.env
 sed -i -e "s/^MONIKER *=.*/MONIKER=$NODENAME/" $HOME/orai.env
-sed -i -e "s/0.41.3/$VERSION/" $HOME/docker-compose.yml
+sed -i -e "s/0.41.4/$VERSION/" $HOME/docker-compose.yml
 
 # Build and enter the container
 docker-compose pull && docker-compose up -d --force-recreate
@@ -69,40 +71,29 @@ docker exec -it orai_node /bin/bash -c 'oraid init $NODENAME --chain-id "$CHAIN_
 #⛔ oraid keys add $NODENAME 2>&1 | tee account.txt && exit
 #       👆 OR 👇
 #👉 import exist wallet (recover wallet)
-docker exec -it orai_node /bin/bash -c 'oraid keys add $NODENAME --recover'
+# ???docker exec -it orai_node /bin/bash -c 'oraid keys add $NODENAME --recover'
 # Enter keyring passphrase:
 
 # Download genesis.json
 wget -O $HOME/.oraid/config/genesis.json https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/genesis.json
 
 
+# Config app
+oraid init $NODENAME --chain-id $CHAIN_ID --home "$HOME/$FOLDER"
+# oraid keys add $WALLET --recover
 
-# Netwrok setup | Replace config.toml and app.toml
-# Set peers and
-PORT="266"
-PEERS=""
-#PEERS="$(curl -sS https://rpc.oraichain.hexnodes.co/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | sed -z 's|\n|,|g;s|.$||')"
-SEEDS=""
+# Download configuration
+curl -Ls https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/genesis.json > $HOME/.oraid/config/addrbook.json
+curl -Ls https://snapshots.nysa.network/Oraichain/addrbook.json > $HOME/.oraid/config/addrbook.json
 
-#🏀 Find/Replace | config.toml 
-sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/.oraid/config/config.toml
-sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|" $HOME/.oraid/config/config.toml
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}58\"%; 
-s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}57\"%; 
-s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}60\"%; 
-s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}56\"%; 
-s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}60\"%" $HOME/.oraid/config/config.toml
+# Set seeds and peers
+sed -E -i 's/seeds = \".*\"/seeds = \"4d0f2d042405abbcac5193206642e1456fe89963@3.134.19.98:26656,24631e98a167492fd4c92c582cee5fd6fcd8ad59@162.55.253.58:26656,bf083c57ed53a53ccd31dc160d69063c73b340e9@3.17.175.62:26656,35c1f999d67de56736b412a1325370a8e2fdb34a@5.189.169.99:26656,5ad3b29bf56b9ba95c67f282aa281b6f0903e921@64.225.53.108:26656,d091cabe3584cb32043cc0c9199b0c7a5b68ddcb@seed.orai.synergynodes.com:26656\"/' $HOME/.oraid/config/config.toml
 
-#🏀 Find/Replace | app.toml
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.001orai\"/" $HOME/.oraid/config/app.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}17\"%; 
-s%^address = \":8080\"%address = \":${PORT}80\"%; 
-s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}90\"%;
-s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}91\"%; 
-s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${PORT}45\"%; 
-s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${PORT}46\"%" $HOME/.oraid/config/app.toml
+# Disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.oraid/config/config.toml
 
-# Set Config Pruning
+# Config pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
@@ -112,24 +103,16 @@ sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_rec
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.oraid/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.oraid/config/app.toml
 
+# Set minimum gas price
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.025orai\"|" $HOME/.oraid/config/app.toml
 
 echo "# fix memory leak issue add this to the bottom of app.toml">> $HOME/.oraid/config/app.toml
 echo "[wasm]" >> $HOME/.oraid/config/app.toml
 echo "query_gas_limit = 300000" >> $HOME/.oraid/config/app.toml
 echo "memory_cache_size = 400" >> $HOME/.oraid/config/app.toml
 
+
 # docker-compose restart orai && docker-compose exec -d orai bash -c 'oraivisor start'
 docker-compose restart orai && docker-compose exec orai bash -c 'oraivisor start --p2p.pex false --p2p.persistent_peers "e6fa2f222236a9ca5e10b238de87eb12497a649c@167.99.119.182:26656,911b290c59a4d3248534b53bdbc8dd4615bb5870@167.99.119.182:26656,35c1f999d67de56736b412a1325370a8e2fdb34a@5.189.169.99:26656,5ad3b29bf56b9ba95c67f282aa281b6f0903e921@64.225.53.108:26656"'
-#✋ 🍺🍺🍺 WAIT UNTIL YOUR NODE IS SYNCHRONIZED 🍺🍺🍺
 
-#👉 Recover node information
-#🛑 /mnt/orai/.oraid/config/priv_validator_key.json
-#🛑 /mnt/orai/.oraid/config/node_key.json
-#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-#       👆 OR 👇
-#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
-#🚀 Create validator transaction (inside of the container)
-#⛔ make 2orai over naar de Vallet
-#⛔ docker-compose exec orai bash
-#⛔ wget -O /usr/bin/fn https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/fn.sh && chmod +x /usr/bin/fn && fn createValidator
-#🔖 〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️
+#
