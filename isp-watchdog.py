50601e7a5f38fc337d52e21504e9bbb42e34c2b3
pulsar node <61687287+celik23@users.noreply.github.com>
2024-04-19 18:18:13 +0200

    Add files via upload

--- isp-watchdog.py
+++ isp-watchdog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # ISP Downtime Monitoring | [pi]-----[router]-----[google]
-# pip install 
+# pip install socket
 import socket
 import time
 from datetime import datetime
@@ -15,19 +15,24 @@ class Server():
 		self.failure = failure
 
 
-	def watchdog(self):
+	def check_connectivity(self):
 		try:
-			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-				sock.settimeout(2)
-				sock.connect((self.host, self.port))
-				return True
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+				s.settimeout(3)
+				s.connect((self.host, self.port))
+			if self.failure:
+				duration = time.time() - self.failure
+				self.write_to_file(str(duration)[:6], '✅UP')
+				self.failure = None
 
-		# except OSError as error:				
-		except socket.error:
-			return False
+		except OSError as error:
+			if not self.failure:
+				self.failure = time.time()
+				self.write_to_file('','❌DOWN')
 
 
-	def write_file(self, duration, msg):
+
+	def write_to_file(self, duration, msg):
 		tm = (datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
 		print(template.format(NAME=self.name, TIME=tm, LENGTH=duration, ERROR=msg))
 
@@ -35,32 +40,21 @@ class Server():
 			f.write(template.format(NAME=self.name, TIME=tm, LENGTH=duration, ERROR=msg+"\n"))
 
 
-	# report only isp or router down!
-	def check_connectivity(self):
-		if self.watchdog():
-			if self.failure:
-				duration = time.time() - self.failure
-				self.write_file(str(duration)[:6], '✅UP')
-				self.failure = None
-		else:
-			if not self.failure:
-				self.failure = time.time()
-				self.write_file('','❌DOWN')
-
 
 if __name__ == "__main__":
 	# check first isp-router second google.nl
-	servers = [Server('router','192.168.2.254',80,None), Server('google','8.8.8.8',53,None)]
-	servers[0].write_file('','Start-Monitorig')
+	servers = [Server('google','8.8.8.8',53,None)]
+	# servers = [Server('router','192.168.2.254',80,None), Server('google','8.8.8.8',53,None)]
+	servers[0].write_to_file('','Start-Monitorig')
 
 	# check more often if there's already an issue.
 	while True:
 		try:
-			time.sleep(0.5)
+			time.sleep(0.2)
 			for server in servers:
 				server.check_connectivity()
 
 		except KeyboardInterrupt:
 			print("\nGood bye! Hope your ISP issues are resolved!")
 			exit()
-# timedatectl 
+# tracert 8.8.8.8
