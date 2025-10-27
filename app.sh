ae4c19adfa5056c12631a1f101a482e57efb3e42
goktengri <61687287+celik23@users.noreply.github.com>
2025-10-27 11:23:01 +0100

    Update app.sh

--- app.sh
+++ app.sh
@@ -10,4 +10,10 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome*.deb
 # sudo apt-get install -f
 
+# Breave
+sudo apt update
+sudo apt install snapd
+sudo snap install snapd
+sudo snap install brave
+
 #vscode
