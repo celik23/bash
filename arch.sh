12d2886dc61219a8312d651d8a9d1bd482cd4884
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-11 10:42:19 +0200

    Update and rename arch to arch.sh

--- arch.sh
+++ arch.sh
@@ -3,12 +3,12 @@
 sudo pacman -S firefox brave chromium gparted
 sudo pacman -S mpv ffmpeg
 sudo pacman -S onlyoffice-bin
-sudo pacman -S libreoffice-fresh
 sudo pacman -S discover htop p7zip
+# sudo pacman -S libreoffice-fresh
 
 paru -S sublime-text
 paru -S onlyoffice-bin
 
-sudo su
-sudo pacman -S refind
-refind-install
+# sudo su
+# sudo pacman -S refind
+# refind-install
