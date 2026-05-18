273b957abc6b0accb3385ef88f1d6e539a1af928
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-18 09:53:36 +0200

    Update and rename arch.sh to cachyos.sh

--- cachyos.sh
+++ cachyos.sh
@@ -1,13 +1,10 @@
 
 #💻 cachyos (arch)
 sudo pacman -S firefox brave chromium gparted
-sudo pacman -S mpv ffmpeg
 sudo pacman -S onlyoffice-bin
 sudo pacman -S discover htop p7zip
 
 paru -S sublime-text
-# paru -S onlyoffice-bin
 
-# sudo su
 # sudo pacman -S refind
 # refind-install
