96a7c616296c0f0ccea5f9a5d2fd636ad687a83c
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-18 09:24:08 +0200

    Update app-install.sh

--- app-install.sh
+++ app-install.sh
@@ -38,22 +38,22 @@ sudo apt install grub-customizer -y
 # sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # sudo apt install ./google-chrome*.deb
 
-echo "# VSCode install"
-echo "# 1. Import Microsoft’s GPG key"
-sudo wget -qO /usr/share/keyrings/microsoft.gpg https://packages.microsoft.com/keys/microsoft.asc
+# echo "# VSCode install"
+# echo "# 1. Import Microsoft’s GPG key"
+# sudo wget -qO /usr/share/keyrings/microsoft.gpg https://packages.microsoft.com/keys/microsoft.asc
 
-echo "# 2. Add the VS Code repository"
-sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null << 'EOF'
-Types: deb
-URIs: https://packages.microsoft.com/repos/code
-Suites: stable
-Components: main
-Architectures: amd64,arm64,armhf
-Signed-By: /usr/share/keyrings/microsoft.gpg
-EOF
+# echo "# 2. Add the VS Code repository"
+# sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null << 'EOF'
+# Types: deb
+# URIs: https://packages.microsoft.com/repos/code
+# Suites: stable
+# Components: main
+# Architectures: amd64,arm64,armhf
+# Signed-By: /usr/share/keyrings/microsoft.gpg
+# EOF
 
-echo "# 3. Update package lists and install VS Code"
-sudo apt install code
+# echo "# 3. Update package lists and install VS Code"
+# sudo apt install code
 
 echo "# Sublime Text"
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
