94d6ed217977279fd459822bb55349a90bf68aa8
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-18 09:25:47 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,10 +4,9 @@ sudo pacman -S firefox brave chromium gparted
 sudo pacman -S mpv ffmpeg
 sudo pacman -S onlyoffice-bin
 sudo pacman -S discover htop p7zip
-# sudo pacman -S libreoffice-fresh
 
 paru -S sublime-text
-paru -S onlyoffice-bin
+# paru -S onlyoffice-bin
 
 # sudo su
 # sudo pacman -S refind
