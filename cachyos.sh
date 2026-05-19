5638b1d32d4b4ea5ee27aa6e695bc2f9e30f9871
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-19 16:11:37 +0200

    Update cachyos.sh

--- cachyos.sh
+++ cachyos.sh
@@ -1,10 +1,31 @@
 
-#💻 cachyos (arch)
-sudo pacman -S firefox brave chromium gparted
+#💻 cachyos: 
+# -S	install/sync packages
+# -y	refresh package databases
+# -u	upgrade alle packages
+sudo pacman -Syu
+
+# Browser
+sudo pacman -S brave chromium keepassxc # firefox
+sudo pacman -S discover htop p7zip gparted filezilla 
+sudo pacman -S nautilus # dolphin krusader thunar pcmanfm
+sudo pacman -S mpv ffmpeg
 sudo pacman -S onlyoffice-bin
-sudo pacman -S discover htop p7zip
 
-paru -S sublime-text
+# sudo pacman -S libreoffice-fresh     # install
+# sudo pacman -R libreoffice-fresh     # revome
 
-# sudo pacman -S refind
-# refind-install
+paru -S onlyoffice-bin
+
+# Text editor
+sudo pacman -S code 
+paru -S sublime-text # > q > y
+
+# Autostart
+printf '#!/bin/bash
+firefox "http://192.168.0.64:8888/" "http://192.168.0.64:3001"
+' > ~/start-browser-link.sh
+
+sudo chmod +x ~/start-browser-link.sh
+# KDE > System > System settings >Autostart > Add New > Login Script > ~/start-browser-link.sh 
+
