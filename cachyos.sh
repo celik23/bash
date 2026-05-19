61d908ca38a62d68b2b6a6e1ca89ed4d1bcbf96b
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-19 17:19:47 +0200

    Update cachyos.sh

--- cachyos.sh
+++ cachyos.sh
@@ -6,20 +6,20 @@
 sudo pacman -Syu
 
 echo "# Browser: brave, chromium keepassxc"
-sudo pacman -S brave chromium keepassxc # firefox
-sudo pacman -S discover htop p7zip gparted filezilla 
-sudo pacman -S nautilus # dolphin krusader thunar pcmanfm
-sudo pacman -S mpv ffmpeg
-sudo pacman -S onlyoffice-bin
+sudo pacman -S -noconfirm brave chromium keepassxc # firefox
+sudo pacman -S -noconfirm discover htop p7zip gparted filezilla 
+sudo pacman -S -noconfirm nautilus # dolphin krusader thunar pcmanfm
+sudo pacman -S -noconfirm mpv ffmpeg
+sudo pacman -S -noconfirm onlyoffice-bin
 
 # sudo pacman -S libreoffice-fresh     # install
 # sudo pacman -R libreoffice-fresh     # revome
 
 echo "# onlyoffice"
-paru -S onlyoffice-bin
+paru -S -noconfirm onlyoffice-bin
 
 echo "# Text editor"
-sudo pacman -S code 
+sudo pacman -S -noconfirm code 
 paru -S sublime-text # > q > y
 
 echo "# Autostart monitoring"
