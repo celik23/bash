eb0e2ad84f0f9dc4e6b0937ed0f04916aab35db5
goktengri <61687287+celik23@users.noreply.github.com>
2026-05-21 13:27:50 +0200

    Update cachyos.sh

--- cachyos.sh
+++ cachyos.sh
@@ -30,16 +30,6 @@ sudo pacman -S --noconfirm \
     mpv \
     ffmpeg
 
-# sudo pacman -S libreoffice-fresh     # install
-# sudo pacman -R libreoffice-fresh     # revome
-
-# alternative file managers:
-# dolphin
-# nautilus
-# krusader
-# thunar
-# pcmanfm
-
 echo "# Only office"
 sudo pacman -S --noconfirm \
     onlyoffice-bin
@@ -54,8 +44,9 @@ printf '#!/bin/bash
 firefox "http://192.168.0.64:8888/" "http://192.168.0.64:3001"
 ' > ~/start-browser-link.sh
 
+echo "# Autostart
 sudo chmod +x ~/start-browser-link.sh
-# GUI: KDE > System > System settings >Autostart > Add New > Login Script > ~/start-browser-link.sh 
+# GUI: KDE > System > System settings > Autostart > Add New > Login Script > ~/start-browser-link.sh 
 printf '[Desktop Entry]
 Exec=/home/kaan/start-browser-link.sh
 Icon=application-x-shellscript
