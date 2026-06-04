78cc755e8355c41c1da044ca381ff342d782d281
goktengri <61687287+celik23@users.noreply.github.com>
2026-06-04 12:34:18 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # // Copyright (C) 2025
+set +e     # Continue on error
 #
 echo "# setup gonme"
-sudo apt update -y
-sudo apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 
 echo "# Setting Monday as the First Day"
 echo 'LC_TIME=nl_NL.UTF-8' | sudo tee -a /etc/default/locale
@@ -15,23 +15,37 @@ sudo sed -i \
     -e 's/^\s*#\?\s*AutomaticLogin\s*=.*/AutomaticLogin = kaan/' \
     /etc/gdm3/daemon.conf
 
-echo "# KDE partition Manager"
-# sudo apt install partitionmanager -y
 
-echo "# GNOME partition editor"
-sudo apt install gparted -y
+# function pacman/paru
+install() {
+    local manager="$1"
+    shift
 
-echo "# File manager dolphin"
-sudo apt install dolphin -y
+    local GREEN='\033[0;32m'
+    local NC='\033[0m'
+    
+    for pkg in "$@"; do
+        echo -e "${GREEN}Installing $manager${NC} $pkg ..."
 
-echo "# File manager krusader"
-sudo apt install krusader -y
+        if [[ "$manager" == "apt" ]]; then
+            sudo apt -install -y "$pkg"
+        else
+            sudo snap -install -y "$pkg"
+        fi
+    done
+}
 
-echo "# Filezilla"
-sudo apt install filezilla -y 
+pac_packages=(
+    snapd gparted dolphin krusader filezilla grub-customizer
+)
 
-echo "# Install Grub Customizer"
-sudo apt install grub-customizer -y
+aur_packages=(
+    snapd code onlyoffice-desktopeditors
+)
+# sudo snap install --classic code
+
+install apt "${pac_packages[@]}"
+install snap "${aur_packages[@]}"
 
 echo "# Sublime Text"
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
@@ -43,25 +57,7 @@ sudo apt parge "libreoffice*" -y
 sudo apt autoremove -y
 sudo apt clean
 
-###
-echo "# Install snap"
-sudo apt install snapd -y
-sudo snap install snapd
 
-echo "# Brave"
-sudo snap install brave
-
-echo "# KeePassXC"
-sudo snap install keepassxc
-
-echo "# Firefox"
-# sudo snap install firefox
-
-echo "# VS code"
-sudo snap install --classic code
-
-echo "# Install onlyoffice"
-sudo snap install onlyoffice-desktopeditors
 
 ###
 echo "# Install Flatpak"
