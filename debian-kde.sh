6dcbd18626eaf5c10269f0504bc742fe828ab9b3
goktengri <61687287+celik23@users.noreply.github.com>
2026-06-14 16:39:21 +0200

    Update debian-kde.sh

--- debian-kde.sh
+++ debian-kde.sh
@@ -66,17 +66,17 @@ EOF
 # --------------------------------------------------
 # Sublime Text repository
 # --------------------------------------------------
-msg "Install Sublime Text"
-sudo mkdir -p /etc/apt/keyrings
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg
-| gpg --dearmor
-| sudo tee /etc/apt/keyrings/sublime.gpg >/dev/null
+# msg "Install Sublime Text"
+# sudo mkdir -p /etc/apt/keyrings
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg
+# | gpg --dearmor
+# | sudo tee /etc/apt/keyrings/sublime.gpg >/dev/null
 
-echo "deb [signed-by=/etc/apt/keyrings/sublime.gpg] https://download.sublimetext.com/ apt/stable/"
-| sudo tee /etc/apt/sources.list.d/sublime-text.list
+# echo "deb [signed-by=/etc/apt/keyrings/sublime.gpg] https://download.sublimetext.com/ apt/stable/"
+# | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-sudo apt update
-sudo apt install -y sublime-text
+# sudo apt update
+# sudo apt install -y sublime-text
 
 # --------------------------------------------------
 # Remove LibreOffice
