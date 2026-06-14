b3adda37b4fd260126088121429f9f8eca32de46
goktengri <61687287+celik23@users.noreply.github.com>
2026-06-14 16:38:08 +0200

    Update debian-kde.sh

--- debian-kde.sh
+++ debian-kde.sh
@@ -34,7 +34,6 @@ sudo apt upgrade -y
 install_packages "${PACKAGES[@]}"
 
 # --------------------------------------------------
-
 # Enable snap
 # --------------------------------------------------
 msg "Enable snap"
