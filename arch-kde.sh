8e706c99dc99f21fde2c378023430ec6dc3ba836
goktengri <61687287+celik23@users.noreply.github.com>
2026-06-16 21:52:41 +0200

    Update arch-kde.sh

--- arch-kde.sh
+++ arch-kde.sh
@@ -9,6 +9,7 @@ PAC_PACKAGES=(
     dolphin kate nano kio-admin git htop flatpak wget curl ark
     gparted keepassxc chromium filezilla mpv code discover
     doublecmd-qt6 krusader rsync ntfs-3g exfatprogs iw
+    spectacle
 )
 
 AUR_PACKAGES=(
