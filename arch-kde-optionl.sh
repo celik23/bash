8ac7048878c976bd3ff56b9e9adee410db619c6d
goktengri <61687287+celik23@users.noreply.github.com>
2026-08-21 14:36:24 +0200

    Add comment about Konsole window process
    Add comment to indicate running all Konsole windows in a single process.

--- arch-kde-optionl.sh
+++ arch-kde-optionl.sh
@@ -7,3 +7,7 @@ kwriteconfig6 --file dolphinrc --group DetailsMode --key PreviewSize 22
 # view mode details
 kwriteconfig6 --file dolphinrc --group Dolphin --key ViewMode 3
 
+# konsole ✅Run all Konsole windows in a single prosess
+
+
+
