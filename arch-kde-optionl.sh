f7cd595392385e8d7217809b7a1d9dfce11539f0
goktengri <61687287+celik23@users.noreply.github.com>
2026-08-21 15:34:42 +0200

    Change desktop icon size to 22
    Set desktop icon size to 22 in Plasma configuration.

--- arch-kde-optionl.sh
+++ arch-kde-optionl.sh
@@ -10,3 +10,9 @@ kwriteconfig6 --file dolphinrc --group Dolphin --key ViewMode 1
 # konsole ✅Run all Konsole windows in a single prosess
 kwriteconfig6 --file konsolerc --group KonsoleWindow --key UseSingleInstance true
 
+# desktop icons size 32
+kwriteconfig6 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc \
+  --group Containments --group 1 --group Applets --group 2 \
+  --group Configuration --key iconSize 22
+
+#
