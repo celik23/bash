8bbfd21b186544a6e03b84a85681d6e5d784dfa2
goktengri <61687287+celik23@users.noreply.github.com>
2026-08-21 14:49:40 +0200

    Change Dolphin view mode and enable single instance in Konsole

--- arch-kde-optionl.sh
+++ arch-kde-optionl.sh
@@ -5,9 +5,8 @@
 kwriteconfig6 --file dolphinrc --group DetailsMode --key PreviewSize 22
 
 # view mode details
-kwriteconfig6 --file dolphinrc --group Dolphin --key ViewMode 3
+kwriteconfig6 --file dolphinrc --group Dolphin --key ViewMode 1
 
 # konsole ✅Run all Konsole windows in a single prosess
-
-
+kwriteconfig6 --file konsolerc --group KonsoleWindow --key UseSingleInstance true
 
