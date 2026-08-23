994e26f0185a53b0669f2d26b4a079e20a7945ff
goktengri <61687287+celik23@users.noreply.github.com>
2026-08-23 07:44:12 +0200

    Fix script execution for arch-kde-optional.sh

--- arch-kde.sh
+++ arch-kde.sh
@@ -156,6 +156,6 @@ EOF
 # --------------------------------------------------
 # Done
 # --------------------------------------------------
-bash <(curl -s https://raw.githubusercontent.com/celik23/bash/main/debian-kde-optional.sh) arg arg
+curl -s https://raw.githubusercontent.com/celik23/bash/main/arch-kde-optional.sh | bash -s -- arg arg
 msg "DONE"
 #
